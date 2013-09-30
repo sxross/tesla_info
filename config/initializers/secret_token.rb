110aca6466a6915f02523d111eecb0092e5e9680
Steve Ross <sxross@gmail.com>
2013-09-30 10:22:44 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeslaRails::Application.config.secret_key_base = '68caa63ea26b4c8b7de92f5ac9856b86a09e3dfddf2915d92a0e7473e5ccdb297cde2d6f5b018aa76d3eee37e798e7ce5605205adeb050c548389176ffa507bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeslaRails::Application.config.session_store :cookie_store, key: '_tesla_rails_session'
